d79d8f5a66903b7c9a3874079089dc2d454a6059
Midzuki-Rey <maribelkova13@yandex.ru>
2024-01-18 16:59:51 +0300

    example1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
+int main()
+{
+    string a;
+    cin >> a;
+    char b = a[0];
+    if (b <= 122 && b >= 97)
+    {
+        b = b - 32;
+    }
+    else
+    {
+        b = b + 32;
+    }
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << b;
 }
